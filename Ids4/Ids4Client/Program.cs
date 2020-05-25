e9a3adf53fc97b537089bd201993257486b5e3a6
huguodong <531035580@qq.com>
2020-05-25 17:20:31 +0800

    new

---
+++ Ids4/Ids4Client/Program.cs
@@ -0,0 +1,87 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace Ids4Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetTokenByPassWord();
+            Console.ReadLine();
+        }
+
+
+        public static async void GetToken()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "Client",    //id
+                ClientSecret = "secret", //pwd
+                Scope = "api1"  //请求的api
+            });
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            else
+            {
+                Console.WriteLine(tokenResponse.Json);
+            }
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/api/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+
+
+        public static async void GetTokenByPassWord()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine($"disco.Error:{disco.Error}");
+                return;
+            }
+            // request token
+            var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "api",
+                ClientSecret = "secret",
+
+                UserName = "alice",
+                Password = "password",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine($"tokenResponse.Error:{tokenResponse.Error}");
+                return;
+            }
+            Console.WriteLine(tokenResponse.Json);
+        }
+
+    }
+}
---
+++ Ids4/TestApi/Controller/IdentityController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
